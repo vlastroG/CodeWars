bd0fb107eca6d64fefc7af350f9c85252aae482b
Vladimir <vlastroganov@gmail.com>
2022-04-09 00:14:34 +0300

    Вынесение решения задачи CamelCase в отдельный статический класс

--- dotnetcore3/Program.cs
+++ dotnetcore3/Program.cs
@@ -2,73 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static dotnetcore3.CamelCase;
 
 public class Program
 {
     public static void Main()
     {
-        string test = "to_camel_case";
+        string test = "owyhvfkwqh-Lithocchrp-arwveiigfb";
         Console.WriteLine(ToCamelCase(test));
 
         Console.ReadLine();
     }
 
-    public static string ToCamelCase(string str)
-    {
-        var words = StringSplitter(str);
-
-        if (Char.IsUpper(words[0][0]))
-        {
-            words[0] = FirstCharUp(words[0]);
-        }
-
-        for (int i = 1; i < words.Length; i++)
-        {
-            words[i] = FirstCharUp(words[i]);
-        }
-
-
-        StringBuilder sb = new StringBuilder();
-        foreach (var item in words)
-        {
-            sb.Append(item);
-        }
-        return sb.ToString();
-    }
-
-    private static string[] StringSplitter(string str)
-    {
-        var words = new List<string>();
-        try
-        {
-            words = str.Split('-').ToList();
-            if (!(words.Count > 1))
-            {
-                try
-                {
-                    words = str.Split('_').ToList();
-                    if (!(words.Count > 1))
-                    {
-                        throw new ArgumentException($"{nameof(str)} содержит меньше 2-х слов.");
-                    }
-                }
-                catch (Exception)
-                {
-                    throw new ArgumentException();
-                }
-            }
-        }
-        catch (Exception)
-        {
-            throw new ArgumentException();
-        }
-
-        return words.ToArray();
-    }
-
-    private static string FirstCharUp(string word)
-    {
-        char letter = Char.ToUpper(word[0]);
-        return letter + word.Substring(1);
-    }
 }
