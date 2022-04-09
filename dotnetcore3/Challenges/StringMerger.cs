da86920f4bc1ae0281a7bbb0814485a5de3087c2
Vladimir <vlastroganov@gmail.com>
2022-04-09 16:38:03 +0300

    Алгоритм с вырезанием частей и сравнением остатка с невырезанной частью

--- dotnetcore3/Challenges/StringMerger.cs
+++ dotnetcore3/Challenges/StringMerger.cs
@@ -8,6 +8,37 @@ namespace dotnetcore3.Challenges
     {
         public static bool isMerge(string s, string part1, string part2)
         {
+            var input = s;
+            var sContainsPart2 = false;
+            for (var i = 0; i < part1.Length; i++)
+            {
+                var index = input.IndexOf(part1[i]);
+                if (index >= 0)
+                {
+                    input = input.Remove(index, 1);
+                }
+                else return false;
+            }
+            sContainsPart2 = part2 == input;
+
+            input = s;
+            var sContainsPart1 = false;
+            for (var i = 0; i < part2.Length; i++)
+            {
+                var index = input.IndexOf(part2[i]);
+                if (index >= 0)
+                {
+                    input = input.Remove(index, 1);
+                }
+                else return false;
+            }
+            sContainsPart1 = part1 == input;
+
+            return sContainsPart1 && sContainsPart2;
+        }
+
+        public static bool isMerge2(string s, string part1, string part2)
+        {
             StringBuilder _s = new StringBuilder(s);
             StringBuilder _part1 = new StringBuilder(part1);
             StringBuilder _part2 = new StringBuilder(part2);
