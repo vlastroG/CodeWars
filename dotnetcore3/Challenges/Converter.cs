4c85ccdd0f8ae483c528d3984632f8ac00208ab3
Владимир Строганов <vlastroganov@gmail.com>
2022-04-11 10:04:19 +0300

    Binary converter

---
+++ dotnetcore3/Challenges/Converter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnetcore3.Challenges
+{
+    internal static class Converter
+    {
+        public static string ToBinary(int n)
+        {
+            return Convert.ToString(n, 2);
+        }
+    }
+}
--- dotnetcore3/Program.cs
+++ dotnetcore3/Program.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using dotnetcore3.Challenges;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using static dotnetcore3.Challenges.StringMerger;
+using static dotnetcore3.Challenges.Converter;
 
 public class Program
 {
     public static void Main()
     {
-        Console.WriteLine(isMerge("Can we merge it? Yes, we can!", "n ee tYw n!", "Cawe mrgi? es, eca"));
-        Console.WriteLine(isMerge("", "", ""));
-        Console.WriteLine(isMerge("codewars", "", "codewars"));
-        
+        //Console.WriteLine(isMerge("Can we merge it? Yes, we can!", "n ee tYw n!", "Cawe mrgi? es, eca"));
+        //Console.WriteLine(isMerge("", "", ""));
+        //Console.WriteLine(isMerge("codewars", "", "codewars"));
+
+        Console.WriteLine(Converter.ToBinary(3));
+        Console.WriteLine(Converter.ToBinary(-3));
 
         Console.ReadLine();
     }
