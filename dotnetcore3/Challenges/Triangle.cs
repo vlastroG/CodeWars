4dd4f6868b9327aaa8e55efdb8d95b362f1c3c91
Владимир Строганов <vlastroganov@gmail.com>
2022-04-11 12:07:08 +0300

    IsTriangle

---
+++ dotnetcore3/Challenges/Triangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnetcore3.Challenges
+{
+    internal static class Triangle
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            return (a + b) > c && (a + c) > b && (b + c) > a;
+        }
+    }
+}
