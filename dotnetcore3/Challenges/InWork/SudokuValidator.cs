7be5624bc212eb24f795cd5c9dbe86c7c7eb3593
Владимир Строганов <vlastroganov@gmail.com>
2022-04-12 10:52:52 +0300

    SudokuValidator class created

---
+++ dotnetcore3/Challenges/InWork/SudokuValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnetcore3.Challenges
+{
+    internal static class SudokuValidator
+    {
+        public static bool ValidateSolution(int[][] board)
+        {
+            //TODO
+        }
+    }
+}
