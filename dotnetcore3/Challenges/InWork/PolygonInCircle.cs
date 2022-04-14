6abce86d57165ca1add7a4d943d13aac1a8fd267
Владимир Строганов <vlastroganov@gmail.com>
2022-04-14 09:53:40 +0300

    Area Of regular PolygonInside Circle

---
+++ dotnetcore3/Challenges/InWork/PolygonInCircle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Math;
+
+namespace dotnetcore3.Challenges.InWork
+{
+    internal class PolygonInCircle
+    {
+        public class Calculator
+        {
+            public static double AreaOfPolygonInsideCircle(double circleRadius, int numberOfSides)
+            {
+                return Math.Round(numberOfSides * circleRadius * circleRadius / 2 * Math.Sin(2 * Math.PI / numberOfSides), 3);
+            }
+        }
+    }
+}
