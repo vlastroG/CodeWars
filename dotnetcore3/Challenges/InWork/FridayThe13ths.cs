e549248f6f5805fd41c3a9d5651b7a85a2204049
Владимир Строганов <vlastroganov@gmail.com>
2022-04-14 18:03:17 +0300

    Friday13Kata beginning

---
+++ dotnetcore3/Challenges/InWork/FridayThe13ths.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace dotnetcore3.Challenges.InWork
+{
+    public class FridayThe13ths
+    {
+        public static string FridayTheThirteenths(int Start, int End = int.MinValue)
+        {
+            DateTime timeStart = new DateTime(Start, 1, 1);
+            var time = timeStart;
+            DateTime timeEnd = new DateTime(End + 1, 1, 1);
+
+            StringBuilder sb = new StringBuilder();
+
+            while (time.Day != timeEnd.Day)
+            {
+                if (time.DayOfWeek == DayOfWeek.Friday && time.Day == 13)
+                {
+                    Console.WriteLine(time.Day);
+                }
+                time += TimeSpan.FromDays(1);
+            }
+            //var days13 = DateTime;
+            var t = (timeEnd - timeStart).Days;
+            return timeStart.ToString();
+        }
+    }
+}
