5b459780e6d6e770122555007cc7f1bcd918a5d7
Владимир Строганов <vlastroganov@gmail.com>
2022-04-18 14:17:33 +0300

    Решение задачи Not very secure

---
+++ dotnetcore3/Challenges/InWork/NotVerySecure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace dotnetcore3.Challenges.InWork
+{
+    public static class NotVerySecure
+    {
+        public static bool Alphanumeric(string str)
+        {
+            if (str == String.Empty)
+                return false;
+            else
+            {
+                var charArr = str.ToLower().ToCharArray();
+                foreach (var item in charArr)
+                {
+                    if (!(item >= 97 && item <= 122 || item >= 48 && item <= 57))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
