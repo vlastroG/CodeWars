29e86293c888aeac79d7fa751130ec7a59654627
Владимир Строганов <vlastroganov@gmail.com>
2022-04-18 12:07:04 +0300

    Remove every 2 element from array

---
+++ BenchMarkTests/RemoveArrItemsTest.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchMarkTests
+{
+    [MemoryDiagnoser]
+    public class RemoveArrItemsTest
+    {
+        private object[] _objs = new object[10000];
+
+        [Benchmark(Baseline = true)]
+        public object[] RemoveEveryOther()
+        {
+            return _objs.Where((e, i) => i % 2 == 0).ToArray();
+        }
+
+        [Benchmark]
+        public object[] MyRemoveEveryOther()
+        {
+            List<object> result = new List<object>();
+            for (int i = 0; i < _objs.Length; i++)
+            {
+                if (i % 2 == 0)
+                    result.Add(_objs[i]);
+            }
+            return result.ToArray();
+        }
+    }
+}
---
+++ dotnetcore3/Challenges/InWork/RemovingElements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dotnetcore3.Challenges.InWork
+{
+    internal static class RemovingElements
+    {
+        public static object[] RemoveEveryOther(object[] arr)
+        {
+            List<object> result = new List<object>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                    result.Add(arr[i]);
+            }
+            return result.ToArray();
+        }
+
+        public static object[] byMaster_RemoveEveryOther(object[] arr)
+        {
+            return arr.Where((e, i) => i % 2 == 0).ToArray();
+        }
+    }
+}
