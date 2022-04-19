c088a439a478e1d6ab54d7f352055cdd8116f470
Владимир Строганов <vlastroganov@gmail.com>
2022-04-19 16:28:37 +0300

    Give me a dimond

---
+++ dotnetcore3/Challenges/InWork/Dimond.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnetcore3.Challenges.CompletedAllTests.WorkWithString
+{
+    public class Dimond
+    {
+        public static string print(int n)
+        {
+            if (n % 2 != 1)
+            {
+                return null;
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append('\n');
+                for (int i = 0, j = n; i < n; i++)
+                {
+                    (j - 1) / 2;
+
+                }
+            }
+        }
+    }
+}
