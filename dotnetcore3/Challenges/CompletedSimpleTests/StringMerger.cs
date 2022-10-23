8f3ef8a2484aed155fbf101288351d9b89b90321
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-23 18:57:55 +0300

    Merged String Checker - attempt again

--- dotnetcore3/Challenges/CompletedSimpleTests/StringMerger.cs
+++ dotnetcore3/Challenges/CompletedSimpleTests/StringMerger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,82 +9,48 @@ namespace dotnetcore3.Challenges
     {
         public static bool isMerge(string s, string part1, string part2)
         {
-            var input = s;
-            var sContainsPart2 = false;
-            for (var i = 0; i < part1.Length; i++)
+            int j = 0;
+            int k = 0;
+            bool lastIsEqual = false;
+            for (int i = 0; i < s.Length; i++)
             {
-                var index = input.IndexOf(part1[i]);
-                if (index >= 0)
+                char sC = s[i];
+                bool theseEqual = j < part1.Length && k < part2.Length && part1[j].Equals(part2[k]);
+                if (j < part1.Length && s[i].Equals(part1[j]))
                 {
-                    input = input.Remove(index, 1);
+                    char p1C = part1[j];
+                    if (theseEqual)
+                    {
+                        j++;
+                        if (j != part1.Length) k++;
+                    }
+                    else
+                    {
+                        j++;
+                        if (lastIsEqual && part2.Length != k) k--;
+                    }
+                    lastIsEqual = theseEqual;
+                    continue;
+                }
+                else if (k < part2.Length && s[i].Equals(part2[k]))
+                {
+                    char p2C = part2[k];
+                    if (theseEqual)
+                    {
+                        j++;
+                        if (j != part1.Length) k++;
+                    }
+                    else
+                    {
+                        k++;
+                        if (lastIsEqual && part1.Length != j) j--;
+                    }
+                    lastIsEqual = theseEqual;
+                    continue;
                 }
                 else return false;
             }
-            sContainsPart2 = part2 == input;
-
-            input = s;
-            var sContainsPart1 = false;
-            for (var i = 0; i < part2.Length; i++)
-            {
-                var index = input.IndexOf(part2[i]);
-                if (index >= 0)
-                {
-                    input = input.Remove(index, 1);
-                }
-                else return false;
-            }
-            sContainsPart1 = part1 == input;
-
-            return sContainsPart1 && sContainsPart2;
-        }
-
-        public static bool isMerge2(string s, string part1, string part2)
-        {
-            StringBuilder _s = new StringBuilder(s);
-            StringBuilder _part1 = new StringBuilder(part1);
-            StringBuilder _part2 = new StringBuilder(part2);
-
-            if ((_part1.Length > 0) && (_s[0] == _part1[0]))
-            {
-                for (int i = 0; i < _s.Length; i++)
-                {
-                    if ((_part1.Length > 0) && (_s[i] == _part1[0]))
-                    {
-                        _part1.Remove(0, 1);
-
-                        continue;
-                    }
-                    else if ((_part2.Length > 0) && (_s[i] == _part2[0]))
-                    {
-                        _part2.Remove(0, 1);
-                        continue;
-                    }
-                    else return false;
-                }
-            }
-
-            else if ((_part2.Length > 0) && (_s[0] == _part2[0]))
-            {
-                for (int i = 0; i < _s.Length; i++)
-                {
-                    if ((_part2.Length > 0) && (_s[i] == _part2[0]))
-                    {
-                        _part2.Remove(0, 1);
-
-                        continue;
-                    }
-                    else if ((_part1.Length > 0) && (_s[i] == _part1[0]))
-                    {
-                        _part1.Remove(0, 1);
-                        continue;
-                    }
-                    else return false;
-                }
-            }
-
-            if ((_part1.Length + _part2.Length) > 0)
-                return false;
-            return true;
+            return (j == part1.Length) && (k == part2.Length);
         }
     }
 }
