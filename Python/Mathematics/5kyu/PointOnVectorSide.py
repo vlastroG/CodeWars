da5629774c828e0a3b5b2d5695df4a98232b70c2
Vladimir Stroganov <vlastroganov@gmail.com>
2023-02-27 00:18:22 +0300

    5 kyu
    Point side on the vector
    https://www.codewars.com/kata/554c8a93e466e794fe000001/python

---
+++ Python/Mathematics/5kyu/PointOnVectorSide.py
@@ -0,0 +1,21 @@
+def point_vs_vector(point, vector):
+    k, b, to_right = find_linear(vector[0], vector[1])
+    function_value = round(k * point[0] + b, 8)
+    point_y = point[1]
+    if point_y < function_value:
+        return 1 if to_right else -1
+    elif point_y == function_value:
+        return 0
+    else:
+        return -1 if to_right else 1
+
+
+def find_linear(point1, point2):
+    k = (point2[1] - point1[1]) / (point2[0] - point1[0])
+    b = point1[1] - point1[0] * (point2[1] - point1[1]) / (point2[0] - point1[0])
+    return k, b, point2[0] > point1[0]
+
+
+vector = [[1201, 9120], [939, 8807]]
+point = -179317, -206537
+print(point_vs_vector(point, vector))
