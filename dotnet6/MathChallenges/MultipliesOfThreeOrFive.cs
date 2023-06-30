7ca9cf17aeef580dd8c9c575dedceacd0b80c087
Vladimir Stroganov <vlastroganov@gmail.com>
2023-06-30 12:03:12 +0300

    Multiplies of 3 or 5
    https://www.codewars.com/kata/514b92a657cdc65150000006/train/csharp

---
+++ dotnet6/MathChallenges/MultipliesOfThreeOrFive.cs
@@ -0,0 +1,18 @@
+﻿namespace dotnet6.MathChallenges {
+    //https://www.codewars.com/kata/514b92a657cdc65150000006/train/csharp
+    internal static class MultipliesOfThreeOrFive {
+        public static int Solution(int value) {
+            int result = 0;
+            for (int i = 0; i < value - 1; i++) {
+                int number = i + 1;
+                if ((number % 3 == 0) || (number % 5 == 0)) {
+                    result += number;
+                }
+            }
+            return result;
+
+            //best practices
+            //return Enumerable.Range(0, n).Where(e => e % 3 == 0 || e % 5 == 0).Sum();
+        }
+    }
+}
---
+++ dotnet6/Program.cs
@@ -0,0 +1,9 @@
+﻿using dotnet6.MathChallenges;
+
+namespace dotnet6 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(MultipliesOfThreeOrFive.Solution(10));
+        }
+    }
+}
